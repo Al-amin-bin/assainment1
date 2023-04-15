ea6add9222e251c6b97c21e64f636af3b9447948
alami <alaminbinshofiqul@gamil.com>
2023-04-15 15:15:12 +0600

    Initial commit

---
+++ bin/assainment1.dart
@@ -0,0 +1,11 @@
+import 'package:assainment1/assainment1.dart' as assainment1;
+
+main(){
+  List <String> phoneNumberList = [
+    "+88", "01768131685", "01768171985", "01768111286", "01768131685"
+  ];
+  print("+88"+phoneNumberList[1]);
+  print("+88"+phoneNumberList[2]);
+  print("+88"+phoneNumberList[3]);
+  print("+88"+phoneNumberList[4]);
+}
